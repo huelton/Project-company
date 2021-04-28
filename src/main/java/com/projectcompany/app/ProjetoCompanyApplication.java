22348387658db939f524edd974531959fc2e291b
huelton <hueltondihon@gmail.com>
2021-04-28 18:21:51 -0300

    first commit, first configuration

---
+++ src/main/java/com/projectcompany/app/ProjetoCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.projectcompany.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCompanyApplication.class, args);
+	}
+
+}
