d96ea2049443f83568dc2072adaa4c0840778cfa
huelton <hueltondihon@gmail.com>
2021-04-29 23:06:35 -0300

    criando pages e templates, arquivos js, css

--- src/main/java/com/projectcompany/app/web/controller/HomeController.java
+++ src/main/java/com/projectcompany/app/web/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	// abrir pagina home
-	@GetMapping("/")
+	// abrindo pagina home
+	@GetMapping("/home")
 	public String home() {
 		return "home";
 	}	
